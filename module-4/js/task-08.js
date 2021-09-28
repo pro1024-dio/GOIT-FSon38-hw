a8e8319c4400bb47ff6341fc238f18fcb9ad88b1
Ivan Dernovyi <idernovoy@gmail.com>
2021-09-29 00:42:21 +0300

    module-4
    all tasks completed

---
+++ module-4/js/task-08.js
@@ -0,0 +1,20 @@
+// Задание
+// Выполни рефакторинг кода так, чтобы функция composeMessage(position) вызывалась методом apply.
+
+
+const orders = [
+  { email: "solomon@topmail.ua", dish: "Burger" },
+  { email: "artemis@coldmail.net", dish: "Pizza" },
+  { email: "jacob@mail.com", dish: "Taco" },
+];
+
+// Пиши код ниже этой строки
+function composeMessage(position) {
+  return `Готовим ${this.dish} для ${this.email}. Ваш заказ ${position}-й в очереди.`;
+}
+
+const messages = [];
+for (let i = 0; i < orders.length; i++) {
+  const msg = composeMessage.apply(orders[i], [i + 1]);
+  messages.push(msg);
+}
